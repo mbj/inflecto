f73b9061183bf4b9ee1e2d9acfc393cb304fc4ee
Ben Moss <ben@mossity.com>
2013-03-18 21:08:30 -0400

    Move class method specs to proper directory for mutant

--- spec/unit/inflecto/class_methods/demodulize_spec.rb
+++ spec/unit/inflecto/class_methods/demodulize_spec.rb
@@ -1,11 +1,13 @@
 require 'spec_helper'
 
-describe Inflecto, '.demodulize' do
-  it 'demodulizes module name: DataMapper::Inflecto => Inflecto' do
-    Inflecto.demodulize(i('DataMapper::Inflecto')).should == 'Inflecto'
-  end
+describe Inflecto do
+  describe '.demodulize' do
+    it 'demodulizes module name: DataMapper::Inflecto => Inflecto' do
+      Inflecto.demodulize('DataMapper::Inflecto').should == 'Inflecto'
+    end
 
-  it 'demodulizes module name: A::B::C::D::E => E' do
-    Inflecto.demodulize(i('A::B::C::D::E')).should == 'E'
+    it 'demodulizes module name: A::B::C::D::E => E' do
+      Inflecto.demodulize('A::B::C::D::E').should == 'E'
+    end
   end
 end
--- spec/unit/inflecto/class_methods/underscore_spec.rb
+++ spec/unit/inflecto/class_methods/underscore_spec.rb
@@ -1,20 +1,22 @@
 require 'spec_helper'
 
-describe Inflecto, '.underscore' do
-  it 'underscores DataMapper as data_mapper' do
-    Inflecto.underscore(i('DataMapper')).should == 'data_mapper'
-  end
+describe Inflecto do
+  describe '.underscore' do
+    it 'underscores DataMapper as data_mapper' do
+      Inflecto.underscore('DataMapper').should == 'data_mapper'
+    end
 
-  it 'underscores Merb as merb' do
-    Inflecto.underscore(i('Merb')).should == 'merb'
-  end
+    it 'underscores Merb as merb' do
+      Inflecto.underscore('Merb').should == 'merb'
+    end
 
-  it 'underscores DataMapper::Resource as data_mapper/resource' do
-    Inflecto.underscore(i('DataMapper::Resource')).should == 'data_mapper/resource'
-  end
+    it 'underscores DataMapper::Resource as data_mapper/resource' do
+      Inflecto.underscore('DataMapper::Resource').should == 'data_mapper/resource'
+    end
 
-  it 'underscores Merb::BootLoader::Rackup as merb/boot_loader/rackup' do
-    Inflecto.underscore(i('Merb::BootLoader::Rackup')).should == 'merb/boot_loader/rackup'
+    it 'underscores Merb::BootLoader::Rackup as merb/boot_loader/rackup' do
+      Inflecto.underscore('Merb::BootLoader::Rackup').should == 'merb/boot_loader/rackup'
+    end
   end
 
   it 'underscores data-mapper as data_mapper' do
