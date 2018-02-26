cae12305947951330701e06de142d9313b90e05d
Andriy Savchenko <andrey@aejis.eu>
2018-02-26 15:36:45 +0200

    Inflecto is dead, long live dry-inflector

--- spec/integration/inflector_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Inflecto, 'underscore' do
-  specify 'allows to create snake_case from CamelCase' do
-    expect(Inflecto.underscore('CamelCase')).to eql('camel_case')
-  end
-end
